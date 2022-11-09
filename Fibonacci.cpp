3c4960373d14105d051d6a84f0a904195f54f672
Rabie Kabbany <rabie.rk98@gmail.com>
2022-11-09 16:52:21 +0200

    add Fibonacci series.

---
+++ Fibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+double fib(double num)
+{
+	if (num < 2)
+		return num;
+	else
+	{
+		double Fibonaccii = fib(num - 1) + fib(num - 2);//Bn=An+Bn=Bn−1+Bn−2
+		return Fibonaccii;
+	}
+}
+int main()
+{
+	int num = 0;
+	cout << "Please enter the number of Fibonacci series. \n";
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		cout << fib(i) << ",";
+
+	}
+
+
+}
